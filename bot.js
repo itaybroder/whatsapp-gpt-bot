712ee32903152b6f0a69e72d85a51c67a49ea0f7
itaybroder <itay@intune.ltd>
2023-06-29 10:27:06 +0300

    Added fine tune

--- bot.js
+++ bot.js
@@ -4,10 +4,9 @@ const { generateResponse, generateSummary } = require('./gpt');
 
 const client = new Client();
 const respondToGroups = false; // Set to true if you want the bot to respond to groups
-const messageLimit = 5; // Set this to the number of messages you want to use for generating the summary
+const messageLimit = 10; // Set this to the number of messages you want to use for generating the summary
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
     qrcode.generate(qr, {small: true});
 });
 
@@ -15,59 +14,41 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', async msg => {
-    try {
-        console.log('MESSAGE RECEIVED', msg.body);
-        const chat = await msg.getChat();
-
-        // Do not reply to group messages if respondToGroups is set to false
-        if (!respondToGroups && chat.isGroup) {
-            return;
-        }
-
-        let history = await fetchChatHistory(chat);
-
-        // Generate a summary from the chat history
-        const summary = await generateSummary(history);
-
-        // Fetch last 5 messages
-        let lastMessages = await chat.fetchMessages({limit: messageLimit});
-
-        // Format history into a prompt for the GPT-3 model
-        let formattedPrompt = formatChatPrompt(summary, lastMessages, msg.fromMe, msg.author);
-
-        const reply = await generateResponse(formattedPrompt);
-        chat.sendMessage(reply);
-    } catch (error) {
-        console.error('Error handling message:', error);
-    }
+client.on('message',  msg => {
+    onMessage(msg)
 });
 
 client.initialize();
 
 async function fetchChatHistory(chat) {
-    let allMessages = await chat.fetchMessages();
-    let totalChars = 0;
-    let messages = [];
-
-    for(let i = allMessages.length-1; i>=0; i--) {
-        let message = allMessages[i];
-        totalChars += message.body.length;
-        if (totalChars > 2000) break; // Stop if messages exceed 2000 chars
-        messages.unshift(message); // Insert message at the beginning of array
-    }
-    
-    return messages;
+    let allMessages = await chat.fetchMessages({limit: messageLimit});
+    return allMessages.reverse();
 }
 
-// Formats chat history into a string for use as a prompt for the GPT-3 model
-function formatChatPrompt(summary, lastMessages, fromMe, author) {
+function formatChatPrompt(summary, lastMessages) {
     let formattedPrompt = `<Summary: ${summary}>\n\n###\n\n`;
-  
+
     let formattedMessages = lastMessages.map((msg) => {
-        let sender = (msg.fromMe === fromMe) ? "Agent" : `Coustomer(${author || msg.contact.number})`;
+        let sender = (msg.fromMe) ? "איתי" : "Friend";
         return `${sender}: ${msg.body}`;
     }).join('\n');
-  
+
+    formattedPrompt+=formattedMessages
     return formattedPrompt + "\n->";
+}
+
+const  onMessage = async (msg) => {
+    try {
+        if (!respondToGroups && msg.isGroup) {
+            return;
+        }
+        const chat = await msg.getChat();
+        let history = await fetchChatHistory(chat);
+        const summary = await generateSummary(history);
+        let formattedPrompt = formatChatPrompt(summary, history);
+        const reply = await generateResponse(formattedPrompt);
+        chat.sendMessage(reply);
+    } catch (error) {
+        console.error('Error handling message:', error);
+    }
 }
--- gpt.js
+++ gpt.js
@@ -9,37 +9,57 @@ const openai = new OpenAIApi(configuration);
 
 const generateSummary = async (chatHistory) => {
   try {
-    const prompt = chatHistory.map(msg => `${msg.fromMe ? 'איתי' : 'נדב'}: ${msg.body}`).join('\n');
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
-      max_tokens: 60, // Adjust this value as needed
-    });
 
-    return completion.data.choices[0].text.trim();
+      // Format conversation text for GPT model
+      let text = chatHistory.map(msg => {
+        let sender = (msg.fromMe) ? "Itay" : "Nadav(Friend)";
+        return `${sender}: ${msg.body}`;
+      }).join('\n');
+
+      // Construct prompt
+      let prompt = `
+          "Given the conversation of Itay and his friend Nadav,
+          ${text}
+          shortly summarize the key points discussed in less than 35 words
+      `;
+
+      const chatCompletion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "system", content: "You are an AI that can summarize very well chat conversations between people"}, {role: "user", content: prompt}],
+      });
+
+    return chatCompletion.data.choices[0].message.content;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
 const generateResponse = async (formattedPrompt) => {
   try {
-    const completion = await openai.createCompletion({
-      model: "your-fine-tuned-model-id", // Replace with your fine-tuned model ID
+    let completion = await openai.createCompletion({
+      model: "davinci:ft-intune:itai-training-2023-06-28-19-55-57",
       prompt: formattedPrompt,
-      max_tokens: 150, 
+      temperature: 0,
+      max_tokens: 250,
     });
+    let response = completion.data.choices[0].text;
+    
+    // Remove "<Media omitted>" from response
+    response = response.replace(/<Media omitted>/g, '');
 
-    // Remove the arrow and newline from the start of the response
-    let response = completion.data.choices[0].text.trim();
+    console.log(response);
+
     if (response.startsWith('->')) {
-      response = response.slice(2);
+      response = response.slice(2).trim();
     }
 
     return response;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
-
+prompt_ex = "<Summary: Itay is tired and studying, while Nadav asks about his day and what he's doing. They inquire about each other's well-being and mention their fathers.>\\n\\n###\\n\\nFriend: תספר לי מה קורה איתך\\nFriend: עייף היום האמת ו\\nאיתי: לומד.. מה איתך\\nFriend: מה אתה עושה היום\\nאיתי: שמע אתה אחלה גבר באמת\\nFriend: אבא סבבה\\nאיתי: בסדר הכל טוב? מה עם אבא\\nFriend: היי אחי, אני אחלה מה איתך\\nאיתי: היי מה קורה\\nFriend: \\n->"
+generateResponse(prompt_ex)
 module.exports = { generateResponse, generateSummary };
+
+
